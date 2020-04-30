af0e36f6de4a0c045d3dd56de6e161929f40c83e
tatandru <48557706+tatandru@users.noreply.github.com>
2020-04-30 17:10:40 +0300

    tested reading data from database

--- Licenta/app/src/main/java/com/example/licenta/MainActivity.java
+++ Licenta/app/src/main/java/com/example/licenta/MainActivity.java
@@ -1,14 +1,47 @@
 package com.example.licenta;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-
+    private TextView romana;
+    private TextView engleza;
+    private FirebaseFirestore db;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        romana=findViewById(R.id.textView);
+        engleza=findViewById(R.id.helloWorld);
+        db = FirebaseFirestore.getInstance();
+        db.collection("cuvinte").document("pronumeRomana").get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                DocumentSnapshot pronumeRomana = task.getResult();
+                List<String> pronume = (List<String>) pronumeRomana.get("pronume");
+                Log.d("myTag", pronume.toString());
+                romana.setText(pronume.get(1));
+                engleza.setText(pronume.get(2));
+            }
+        });
+
+
+
+
     }
 }
